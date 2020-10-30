d922107c2a3b11738596c6d3e4855bb0e8b12df4
vhollo <vhollo@protonmail.com>
2020-10-30 10:28:03 +0100

    footer

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,7 +6,6 @@
 
 <script>
 	export let segment
-
 </script>
 
 <svelte:head>
@@ -18,6 +17,32 @@
 	<slot />
 </main>
 
+<footer>
+	<a name="logo" sapper-noscroll href="https://hbs.hu" rel="noopener" target="_blank">
+		<img loading="lazy" src="uploads/hbs-logo-medical.png" alt="hbs logo">
+	</a>
+</footer>
+
 <Cookies/>
 
 <Stylesheet />
+
+<style>
+	footer {
+		/* position: sticky;
+		bottom: 0;
+		background: var(--light);
+		font-size: smaller; */
+		/* display: flex;
+		align-items: center; */
+		/* padding: 0 .5rem;
+		z-index: 1; */
+	}
+	img {
+		max-height: 3rem;
+		width: auto;
+		filter: drop-shadow(2px 2px 0 white);
+		margin: 1rem auto;
+	}
+
+</style>
