97f569285a55e69be32d573f76fb4d38131fa69f
vhollo <vhollo@protonmail.com>
2020-11-03 19:02:31 +0100

    meta

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -9,6 +9,10 @@
 </script>
 
 <svelte:head>
+	<meta name="description"
+		content="Clungene COVID-19 szerológiai (immunológiai) és antigén gyorsteszt kazetta – olcsón, azonnali szállítással" />
+	<meta name="keywords" content="COVID-19 szerológiai és antigén-gyorsteszt, covid-19, clungene, sars-cov-2, ag teszt, igg/igm teszt, antigén,
+		szerológiai, gyorsteszt" />
 </svelte:head>
 
 <Nav {segment} />
