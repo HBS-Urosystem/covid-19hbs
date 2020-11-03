263224306020b3f5ea4bc6d8605e6cef60b84ddc
vhollo <vhollo@protonmail.com>
2020-11-03 23:46:00 +0100

    logo no-noscroll

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -27,7 +27,7 @@
 </header>
 {:else}
 <header>
-	<a name="logo" sapper-noscroll href="{pages.index[$lang].slug}">
+	<a name="logo" href="{pages.index[$lang].slug}">
 		<img loading="lazy" src="uploads/hbs-logo-medical.png" alt="hbs logo">
 	</a>
 	<h1>{@html $tagline}</h1>
