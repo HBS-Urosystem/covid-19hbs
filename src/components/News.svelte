a8fa044a0c6e3d5bd42dcc636811daad73b7c1b4
vhollo <now@civilcontrol.org>
2021-07-19 19:21:56 +0200

    news fix

--- src/components/News.svelte
+++ src/components/News.svelte
@@ -24,19 +24,24 @@
 	}
 </script>
 
-<!--{#if post.list && post.list.length}-->
 {#each post.list as item, i}
 	<article>
+		{#if item.title}
 		<h3>{item.title}</h3>
+		{/if}
 		{@html _md(item.text)}
+		{#if item.link}
 		<button><a href="{item.link}">Részletek</a></button>
+		{/if}
 	</article>
 	{#if (i+1) / 3 == Math.floor((i+1) / 3)}
 		<CTA/>
 	{/if}
 {/each}
+<!--{#if Math.floor((i+1) / 3) == 2}
+<CTA/>
+{/if}-->
 <!--<CTA />-->
-<!--{/if}-->
 
 <style>
 	button {
